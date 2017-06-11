e839af4736f3599ec3f605a9541b7a4e3ead2f27
Peter Dobos <dvc1201@gmail.com>
2017-06-11 10:07:30 +0200

    Adding Se74Report

---
+++ Se74.Net/Report/Se74Report.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Se74.Net.Report
+{
+    public class Se74Report
+    {
+    }
+}
