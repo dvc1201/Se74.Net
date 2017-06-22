ce04f833ab10b2ea49ebea35f72e136617c8880f
dvc1201 <dvc1201@gmail.com>
2017-06-23 00:49:54 +0200

    Adding SelectTag

---
+++ Se74.Net/Elements/SelectTag.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace Se74.Net.Elements
+{
+    public class SelectTag : Se74Element
+    {
+        public SelectTag(By by, string name = "", string comment = "") : base(by, name, comment)
+        {
+        }
+    }
+}
